037a5a1c99ea08b55deec442982a87f419277112
Christian Gutierrez <Person1080p@gmail.com>
2022-06-28 22:10:35 -0400

    i give up for now on bash

--- malRateScrape.sh
+++ malRateScrape.sh
@@ -1,2 +1,6 @@
-#!/bin/bash/
-# 
+#!/bin/bash
+wget -q https://myanimelist.net/anime/$1 -O page.html
+tail -n +22 page.html | head -n $(( 1 )) > out.txt
+# lynx https://myanimelist.net/anime/1
+# grep -o -E "[a-zA-Z0-9]*" page.txt >> word.txt
+# tr '[:upper:]' '[:lower:]' < word.txt > result.txt
